e9a23e4532401f9326a74348c86b26b90e0b541e
Joan Nicolau <joannicolau23@gmail.com>
2022-05-07 13:06:01 +0200

    add vegetable entity, implementing active record pattern

--- src/main/java/edu/poniperro/Vegetable.java
+++ src/main/java/edu/poniperro/Vegetable.java
@@ -9,6 +9,7 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
  * we are using the active record pattern with panache, hibernate orm helper
  * note: extending from panache entity
  * ofrece un id por defecto
+ * no es necesario getters and setters  
  */
 
 @Entity
