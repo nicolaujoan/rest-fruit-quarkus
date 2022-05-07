f4363e79c186e116e78dd0466e188fe0e4c560f5
Joan Nicolau <joannicolau23@gmail.com>
2022-05-07 14:51:04 +0200

    add starting script to initilize db

---
+++ sql/start.sql
@@ -0,0 +1,45 @@
+-- delete tables 
+DROP TABLE IF EXISTS vegetable;
+DROP TABLE IF EXISTS fruit;
+DROP TABLE IF EXISTS legume;
+
+-- create tables again
+CREATE TABLE vegetable (
+    id serial PRIMARY KEY,
+    name VARCHAR UNIQUE NOT NULL, 
+    description VARCHAR UNIQUE NOT NULL
+);
+
+CREATE TABLE fruit (
+    id serial PRIMARY KEY,
+    name VARCHAR UNIQUE NOT NULL, 
+    description VARCHAR UNIQUE NOT NULL
+);
+
+CREATE TABLE legume (
+    id serial PRIMARY KEY,
+    name VARCHAR UNIQUE NOT NULL, 
+    description VARCHAR UNIQUE NOT NULL
+);
+
+-- insert initial values
+INSERT INTO vegetable(name, description)
+VALUES
+('onion', 'no baby no cry'),
+('tomatoe', 'red and nice in a salad'),
+('lettuce', 'always in your burger'),
+('pepper', 'red and hot for chili');
+
+INSERT INTO fruit(name, description)
+VALUES
+('apple', 'red or green but clean'),
+('pomelo', 'nobody likes it'),
+('orange', 'nice for a morning juice'),
+('melon', 'fresh in summer');
+
+INSERT INTO legume(name, description)
+VALUES
+('bean', 'java uses it a lot'),
+('peanut', 'your elephant loves it'),
+('lentil', 'metal 4 you ma friend'),
+('chickpeas', 'your head is like it');
