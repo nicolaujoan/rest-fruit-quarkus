a89e4308e19562c53617c390fd5e23e3a33d5c5c
Joan Nicolau <joannicolau23@gmail.com>
2022-05-08 18:53:22 +0200

    refactor: create fruitService and call it in the resource

--- src/main/java/edu/poniperro/FruitResource.java
+++ src/main/java/edu/poniperro/FruitResource.java
@@ -1,40 +1,44 @@
 package edu.poniperro;
 
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import java.net.URI;
+import edu.poniperro.services.FruitService;
+
 import java.util.List;
 
 @Path("fruit")
 public class FruitResource {
+
+    @Inject
+    FruitService fruitService;
     
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Fruit> list() {
-        return Fruit.listAll();
+        return fruitService.listFruits();
     }
 
     @POST
     @Transactional
     @Consumes(MediaType.APPLICATION_JSON)
     public Response add(Fruit fruit) {
-        fruit.persist();
-        return Response.created(URI.create("/fruit" + fruit.id))
-            .build();
+        return fruitService.addFruit(fruit);
     }
 
     @DELETE
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response delete(Fruit fruit) {
-        Fruit.delete("id", fruit.id);
-        return Response.status(202, MediaType.APPLICATION_JSON).entity(fruit).build();
+    @Path("{id}")
+    public Response delete(@PathParam("id") Long id) {
+        return fruitService.deleteFruitById(id);
     }
 }
---
+++ src/main/java/edu/poniperro/services/FruitService.java
@@ -0,0 +1,29 @@
+package edu.poniperro.services;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import edu.poniperro.Fruit;
+
+@ApplicationScoped
+public class FruitService {
+    
+    public List<Fruit> listFruits() {
+        return Fruit.listAll();
+    }
+
+    public Response addFruit(Fruit fruit) {
+        fruit.persist();
+        return Response.created(URI.create("/fruit" + fruit.id))
+        .build();
+    }
+
+    public Response deleteFruitById(Long id) {
+        Fruit.deleteById(id);
+        return Response.status(202, MediaType.APPLICATION_JSON).build();
+    }
+}
