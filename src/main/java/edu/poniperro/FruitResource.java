4cc52d08755906b74ec3f3e28da9b3260ead5249
Joan Nicolau <joannicolau23@gmail.com>
2022-05-08 13:18:07 +0200

    modify crud to interact with the db

--- src/main/java/edu/poniperro/FruitResource.java
+++ src/main/java/edu/poniperro/FruitResource.java
@@ -1,8 +1,6 @@
 package edu.poniperro;
 
-import java.util.Set;
-import java.util.LinkedHashSet;
-
+import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -12,34 +10,31 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import java.net.URI;
+import java.util.List;
 
-@Path("/fruits")
+@Path("fruit")
 public class FruitResource {
     
-    private Set<Fruit> fruits = new LinkedHashSet<Fruit>();
-
-    public FruitResource() {
-        fruits.add(new Fruit("Apple", "Winter fruit"));
-        fruits.add(new Fruit("Mango", "Tropical fruit"));
-    }
-
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Set<Fruit> list() {
-        return fruits;
+    public List<Fruit> list() {
+        return Fruit.listAll();
     }
 
     @POST
+    @Transactional
     @Consumes(MediaType.APPLICATION_JSON)
     public Response add(Fruit fruit) {
-        fruits.add(fruit);
-        return Response.status(201, MediaType.APPLICATION_JSON).entity(fruits).build();
+        fruit.persist();
+        return Response.created(URI.create("/fruit" + fruit.id))
+            .build();
     }
 
     @DELETE
     @Consumes(MediaType.APPLICATION_JSON)
     public Response delete(Fruit fruit) {
-        fruits.remove(fruit);
-        return Response.status(202, MediaType.APPLICATION_JSON).entity(fruits).build();
+        Fruit.delete("id", fruit.id);
+        return Response.status(202, MediaType.APPLICATION_JSON).entity(fruit).build();
     }
 }
