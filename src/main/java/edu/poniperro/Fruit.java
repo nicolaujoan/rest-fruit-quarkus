d89349e5393a64ce4c6b6d9f650bc0c5b899f9e0
Joan Nicolau <joannicolau23@gmail.com>
2022-05-08 13:17:32 +0200

    create panache entity using active record

--- src/main/java/edu/poniperro/Fruit.java
+++ src/main/java/edu/poniperro/Fruit.java
@@ -1,45 +1,16 @@
 package edu.poniperro;
 
-public class Fruit {
+import javax.persistence.Column;
+import javax.persistence.Entity;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity
+public class Fruit extends PanacheEntity {
+
+    @Column(unique = true)
     public String name;
+
+    @Column
     public String description;
-    
-    public Fruit(){}  // required by json serialization layer
-
-    public Fruit(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((description == null) ? 0 : description.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Fruit other = (Fruit) obj;
-        if (description == null) {
-            if (other.description != null)
-                return false;
-        } else if (!description.equals(other.description))
-            return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        return true;
-    }
 }
