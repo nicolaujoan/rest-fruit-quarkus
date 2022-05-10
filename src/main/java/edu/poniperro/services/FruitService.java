eec9f8e36e711791be2a4d62f2534887bf5003fa
Joan Nicolau <joannicolau23@gmail.com>
2022-05-10 16:49:22 +0200

    inject fruitRepository and call its logic instead of the entity

--- src/main/java/edu/poniperro/services/FruitService.java
+++ src/main/java/edu/poniperro/services/FruitService.java
@@ -1,28 +1,33 @@
 package edu.poniperro.services;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.core.Response;
+
 import java.net.URI;
 import java.util.List;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.core.Response;
-
 import edu.poniperro.domain.Fruit;
+import edu.poniperro.repositories.FruitRepository;
 
 @ApplicationScoped
 public class FruitService {
     
+    @Inject
+    FruitRepository fruitRepository;
+
     public List<Fruit> listFruits() {
-        return Fruit.listAll();
+        return fruitRepository.listFruits();
     }
 
     public Response addFruit(Fruit fruit) {
-        fruit.persist();
-        return Response.created(URI.create("/fruit" + fruit.id))
+        fruitRepository.addFruit(fruit);
+        return Response.created(URI.create("/fruit" + fruit.getId()))
         .build();
     }
 
     public List<Fruit> deleteFruitById(Long id) {
-        Fruit.deleteById(id);
+        fruitRepository.deleteById(id);
         return this.listFruits();
     }
 }
