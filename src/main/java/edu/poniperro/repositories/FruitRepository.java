b1f2a0d4daea7e5df9002843595731a1dc1e8da8
Joan Nicolau <joannicolau23@gmail.com>
2022-05-10 16:47:38 +0200

    create fruit repository

---
+++ src/main/java/edu/poniperro/repositories/FruitRepository.java
@@ -0,0 +1,30 @@
+package edu.poniperro.repositories;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import edu.poniperro.domain.Fruit;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import java.util.List;
+
+/**
+ * este es nuestro repository, implementa PanacheRepository
+ * colocamos la lógica de las queries en forma de métodos de instancia
+ * a diferencia de active record que son estaticos
+ */
+
+@ApplicationScoped
+public class FruitRepository implements PanacheRepository<Fruit> {
+    
+    public List<Fruit> listFruits() {
+        return listAll();
+    }
+
+    public void addFruit(Fruit fruit) {
+        persist(fruit);
+    }
+
+    public void deleteFruit(Long id) {
+        delete("id", id);
+    }
+}
